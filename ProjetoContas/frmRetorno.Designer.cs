6cadee30fb1c976d640019d135104b63961f584e
Jhonatancs <jdc_santos@outlook.com>
2019-12-02 19:40:46 -0300

    adicionando form de retorno

---
+++ ProjetoContas/frmRetorno.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ProjetoContas
+{
+    partial class frmRetorno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contasDataSet = new ProjetoContas.contasDataSet();
+            this.tb_contaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tb_contaTableAdapter = new ProjetoContas.contasDataSetTableAdapters.tb_contaTableAdapter();
+            this.tableAdapterManager = new ProjetoContas.contasDataSetTableAdapters.TableAdapterManager();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.contasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_contaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contasDataSet
+            // 
+            this.contasDataSet.DataSetName = "contasDataSet";
+            this.contasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tb_contaBindingSource
+            // 
+            this.tb_contaBindingSource.DataMember = "tb_conta";
+            this.tb_contaBindingSource.DataSource = this.contasDataSet;
+            // 
+            // tb_contaTableAdapter
+            // 
+            this.tb_contaTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.tb_administradorTableAdapter = null;
+            this.tableAdapterManager.tb_contaTableAdapter = this.tb_contaTableAdapter;
+            this.tableAdapterManager.tb_usuarioTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = ProjetoContas.contasDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo";
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Location = new System.Drawing.Point(148, 46);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(250, 20);
+            this.txtArquivo.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(148, 111);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(323, 111);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // frmRetorno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 158);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmRetorno";
+            this.Text = "frmRetorno";
+            this.Load += new System.EventHandler(this.frmRetorno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_contaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private contasDataSet contasDataSet;
+        private System.Windows.Forms.BindingSource tb_contaBindingSource;
+        private contasDataSetTableAdapters.tb_contaTableAdapter tb_contaTableAdapter;
+        private contasDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
---
+++ ProjetoContas/frmRetorno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjetoContas
+{
+    public partial class frmRetorno : Form
+    {
+        public frmRetorno()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_contaBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tb_contaBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contasDataSet);
+
+        }
+
+        private void frmRetorno_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'contasDataSet.tb_conta'. Você pode movê-la ou removê-la conforme necessário.
+            this.tb_contaTableAdapter.Fill(this.contasDataSet.tb_conta);
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            StreamReader arq = new StreamReader(txtArquivo.Text);
+
+            string linha;
+
+            while (!arq.EndOfStream)
+            {
+                linha = arq.ReadLine();
+                if (linha.Substring(0,1) == "1" && linha.Substring(108,2) == "06")
+                {
+                    string dtPag = linha.Substring(110,2) + "/"+ linha.Substring(112,2)+"/"+ linha.Substring(114,2);
+                    decimal vlPag = decimal.Parse(linha.Substring(253,13)) / 100;
+                    int cdDoc = int.Parse(linha.Substring(116,10));
+
+                    tb_contaTableAdapter.BaixaPagamento(dtPag, vlPag, cdDoc);
+                }
+            }
+
+            arq.Close();
+        }
+    }
+}
